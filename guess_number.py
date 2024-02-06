3868dbd27523e54747784bcd2fa85c90df2931b6
Hema Dalavayi <gayathri.dhs@gmail.com>
2024-02-06 04:20:51 -0600

    Added a hint for the user if the number guessed was too high or low than the expected number

--- guess_number.py
+++ guess_number.py
@@ -3,6 +3,13 @@ number = 10
 print("I'm thinking of a number...")
 guess = int(input("What number am I thinking of? "))
 
+if int(guess) != number:
+      if int(guess) > number:
+        print("Your guess was higher than the expected number")
+      if int(guess) < number:
+        print('Your guess was lower than the expected number')
+
+
 
 if guess == number:
    print("Congratulations! You guessed the right number.")
@@ -15,4 +22,8 @@ else:
     if int(guess) == number:
       print("Congratulations! You guessed the right number.")
       break
-
+    if int(guess) != number:
+      if int(guess) > number:
+        print("Your guess was higher than the expected number")
+      if int(guess) < number:
+        print('Your guess was lower than the expected number')
